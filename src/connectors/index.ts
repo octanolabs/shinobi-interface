bdb7447d119a0b93149eeb4c3113253e59586626
iquidus <luke@iquidus.io>
2022-04-13 22:54:13 -0400

    hard code appLogoUrl for walletlink connectors

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -2,7 +2,6 @@ import { Web3Provider } from '@ethersproject/providers'
 import { InjectedConnector } from '@web3-react/injected-connector'
 import { WalletLinkConnector } from '@web3-react/walletlink-connector'
 import { NetworkConnector } from './NetworkConnector'
-import SHINOBI_LOGO_URL from '../assets/svg/logo.svg'
 
 const NETWORK_URL = process.env.REACT_APP_NETWORK_URL
 
@@ -28,6 +27,5 @@ export const injected = new InjectedConnector({
 export const walletlink = new WalletLinkConnector({
   url: 'https://rpc.octano.dev',
   appName: 'Shinobi',
-  appLogoUrl: SHINOBI_LOGO_URL,
-  supportedChainIds: [8],
+  appLogoUrl: 'https://shinobi.ubiq.ninja/favicon.png',
 })
